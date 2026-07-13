848c47bc9c7efd5bd603e07e8bbd97048a3881b9
Siavashi <siavashi3899@gmail.com>
2026-07-13 20:27:29 +0430

    Export background code as component

---
+++ components/main-header/main-header.js
@@ -0,0 +1,31 @@
+import Link from "next/link";
+import Image from "next/image";
+import MainHeaderBackground from "./main-header-background";
+import logoImg from "@/assets/logo.png";
+import classes from "./main-header-style.module.css";
+
+export default function MainHeader() {
+  return (
+    <>
+      <MainHeaderBackground />
+
+      <header className={classes.header}>
+        <Link href="/" className={classes.logo}>
+          <Image src={logoImg} alt="A table with dishes of food" priority />
+          <p>NextLevel Food</p>
+        </Link>
+
+        <nav className={classes.nav}>
+          <ul>
+            <li>
+              <Link href="/meals">Browse Meals</Link>
+            </li>
+            <li>
+              <Link href="/community">Foodies Community</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
+}
