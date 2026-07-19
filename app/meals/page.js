49cd1c44209d8b53cc7e621c4ccb008da989926d
Siavashi <siavashi3899@gmail.com>
2026-07-19 22:40:56 +0430

    Add meals to meals page

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,7 +1,9 @@
 import Link from "next/link";
 import classes from "./page.module.css";
-import MealsGrid from "@/components/meals/meals.grid";
-export default function MealsPage() {
+import MealsGrid from "@/components/meals/meals-grid";
+import { getMeals } from "@/lib/meals";
+export default async function MealsPage() {
+  const meals = await getMeals();
   return (
     <>
       <header className={classes.header}>
@@ -16,8 +18,8 @@ export default function MealsPage() {
           <Link href="/meals/share">Share Your Favorite Recipe</Link>
         </p>
       </header>
-      <main>
-        <MealsGrid meals={[]} />
+      <main className={classes.main}>
+        <MealsGrid meals={meals} />
       </main>
     </>
   );
--- components/meals/meals-grid.js
+++ components/meals/meals-grid.js
@@ -2,7 +2,7 @@ import classes from "./meals-grid.module.css";
 import MealItem from "./meal-item";
 export default function MealsGrid({ meals }) {
   return (
-    <ul className={meals}>
+    <ul className={classes.meals}>
       {meals.map((meal) => (
         <li key={meal.id}>
           <MealItem {...meal} />
