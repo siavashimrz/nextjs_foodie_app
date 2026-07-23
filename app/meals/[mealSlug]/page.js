1c161c393cadf79bca0adb6a125ba03fab0903de
Siavashi <siavashi3899@gmail.com>
2026-07-23 20:18:48 +0430

    Add not-found.js for meal

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -1,8 +1,38 @@
+import { getMeal } from "@/lib/meals";
+import classes from "./page.module.css";
+import Image from "next/image";
+import { notFound } from "next/navigation";
 export default async function MealDetailPage({ params }) {
-  const { slug } = await params;
+  const { mealSlug } = await params;
+  console.log("---------" + mealSlug);
+  const meal = await getMeal(mealSlug);
+
+  if (!meal) {
+    notFound();
+  }
+
   return (
-    <main>
-      <h1 style={{ color: "white", textAlign: "center" }}>{slug}</h1>
-    </main>
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image src={meal.image} alt={meal.title} fill />
+        </div>
+        <div className={classes.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={classes.creator}>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={classes.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main>
+        <p
+          className={classes.instructions}
+          dangerouslySetInnerHTML={{
+            __html: meal.instructions.replace(/\n/g, "<br />"),
+          }}
+        ></p>
+      </main>
+    </>
   );
 }
